bcc44c909c7d9c5a12aedb6b56a460c922fd52a4
oogway7h <140742249+oogway7h@users.noreply.github.com>
2025-11-05 15:02:41 -0400

    desplegado

--- config/settings.py
+++ config/settings.py
@@ -49,13 +49,7 @@ MIDDLEWARE = [
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
-    "https://clinicavisionweb.vercel.app",
-    "https://clinica-frontend-six.vercel.app",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-    "https://clincavisionex.netlify.app",
-    "https://clinicavisionweb.vercel.app",
+    "https://clinicavisionwebs.vercel.app",
     "https://clinicavisionwebs-m7wjvvwr1-carlos-otsubos-projects.vercel.app",
 
 ]
@@ -63,6 +57,17 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_HEADERS = [
     "accept", "accept-encoding", "authorization", "content-type",
     "dnt", "origin", "user-agent", "x-csrftoken", "x-requested-with",
+]
+
+CORS_ALLOW_ALL_ORIGINS = False   # True solo para pruebas rápidas
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
 ]
 
 ROOT_URLCONF = 'config.urls'
