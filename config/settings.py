f10c8360d4db3eea0e89dcfcb783d056c9630177
oogway7h <140742249+oogway7h@users.noreply.github.com>
2025-11-26 15:05:14 -0400

    requirements.txt actualizado

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://clincavisionex.netlify.app",
     "https://clinicavisionweb.vercel.app",
+    "https://clinica-vision-hh1dj4n7y-carlos-otsubos-projects.vercel.app"
 
 ]
 
